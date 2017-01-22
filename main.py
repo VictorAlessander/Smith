f070f0dac4a1ef441466a6d7655b149d144e8606
Victor Alessander <alessander@protonmail.com>
2017-01-22 14:36:12 -0300

    Function of string treatment changed to function re.sub

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 
 
 try:
-	import cria_planilha
-	import time
-	from bs4 import BeautifulSoup as BSoup
-	import requests
-	from openpyxl import Workbook, load_workbook
+    import cria_planilha
+    import time, re
+    from bs4 import BeautifulSoup as BSoup
+    import requests
+    from openpyxl import Workbook, load_workbook
 
 except Exception as e1:
-	print('[!] Ocorreu um erro: {}' .format(e1))
+    print('[!] Ocorreu um erro: {}' .format(e1))
 
 
 """
@@ -43,135 +43,144 @@ Digite help para mais informações.
 """)
 
 
-def main():
+#def avaliar(preco, a_vista):
 
-	global planilha, nome_aba, nome_arquivo, ws, wb
+#        filter2 = lambda *argv: [re.sub('[R$ deporàvista]', '', arg) for arg in argv]
+#        filter3 = lambda *argv: [re.sub('[,]', '.', arg) for arg in argv]
 
+#        p, a = filter2(preco, a_vista)
+#        p, a = filter3(preco, a_vista)
 
-	planilha = str(input("Insira o nome da planilha (Não esqueça de colocar a extensão .xlsx)\n"))
-
-	try:
-		wb = load_workbook(planilha) # Carrega um arquivo xlsx existente
-
-	except FileNotFoundError:
-		print(green + "[+] Criando planilha. . ." + restore)
-		cria_planilha.Planilha(planilha).criar()
-		wb = load_workbook(planilha) # Carrega um arquivo xlsx existente
-
-	opcao = str.lower(input("[*] Deseja criar uma nova aba para inserir os dados? [S/N/C]\n"))
-
-	if opcao == 's':
-		nome_aba = str(input("Digite um nome para a nova aba: "))
-		ws = wb.create_sheet(nome_aba)
-
-	elif opcao == 'n':
-		nome_aba = str(input("Digite o nome da aba: "))
-		ws = wb.get_sheet_by_name(nome_aba)
-
-	else:
-		print(yellow + "[!] Escrevendo em uma aba existente. . ." + restore)
-		ws = wb.active
-
-	nome_arquivo = str(input("Nome do arquivo com os links (Não esqueça de colocar a extensão .txt)\n"))
-	print(green + '[+] Iniciando' + restore)
-	time.sleep(2)
+#        print(p, a)
 
 
 def extrair():
 
-	try:
-		a1 = open(nome_arquivo, 'r') # Lê o arquivo com os links
+    try:
+        a1 = open(nome_arquivo, 'r') # Lê o arquivo com os links
 
-		for x in a1.readlines():
+        for x in a1.readlines():
 
-			links = x
+            links = x
 
-			# Variáveis que receberão as strings brutas
+            # Variáveis que receberão as strings brutas
 
-			nome = ""
-			preco_antigo = ""
-			preco = ""
-			a_vista = ""
+            nome = ""
+            preco_antigo = ""
+            preco = ""
+            a_vista = ""
 
-			open_link = requests.get(links) # Abre o link informado na variável link
+            open_link = requests.get(links) # Abre o link informado na variável link
 
-			soup = BSoup(open_link.content, "html.parser") # Faz o parser das informações no link
+            soup = BSoup(open_link.content, "html.parser") # Faz o parser das informações no link
 
-			results = [] # Array que vai receber as strings tratadas
+            results = [] # Array que vai receber as strings tratadas
 
-			# Procura nas linhas em html as tags/classes e o seu valor atribui a string nome
+            # Procura nas linhas em html as tags/classes e o seu valor atribui a string nome
 
-			x = soup.find('h1', attrs={'class' : 'titulo_det'})
+            w = soup.find('h1', attrs={'class' : 'titulo_det'})
 
-			if x:
-				nome = x.text
+            if w:
+                nome = w.text
 
-			w = soup.find('div', attrs={'class' : 'preco_antigo'})
+            x = soup.find('div', attrs={'class' : 'preco_antigo'})
 
-			if w:
-				preco_antigo = w.text
+            if x:
+                preco_antigo = x.text
 
-			y = soup.find('div', attrs={'class' : 'preco_normal'})
+            y = soup.find('div', attrs={'class' : 'preco_normal'})
 
-			if y:
-				preco = y.text
+            if y:
+                preco = y.text
 
-			z = soup.find('span', attrs={'class' : 'preco_desconto'})
+            z = soup.find('span', attrs={'class' : 'preco_desconto'})
 
-			if z:
-				a_vista = z.text
+            if z:
+                a_vista = z.text
 
+            #Retira os caracteres especificados (tratamento da string)
+            
+            filter1 = lambda *argv: [re.sub('[\t\n]', '', arg) for arg in argv]
 
-			#Retira os caracteres especificados (tratamento da string)
+            preco_antigo, preco, a_vista = filter1(preco_antigo, preco, a_vista)
 
-			preco_antigo = "".join(preco_antigo.split('\t'))
-			preco_antigo = "".join(preco_antigo.split('\n'))
-			preco = "".join(preco.split('\t'))
-			preco = "".join(preco.split('\n'))
-			a_vista = "".join(a_vista.split('\t'))
-			a_vista = "".join(a_vista.split('\n'))
+            #Adiciona ao array results a string tratada
 
-			#Adiciona ao array results a string tratada
+            results.append(nome)
+            results.append(preco_antigo)
+            results.append(preco)
+            results.append(a_vista)
 
-			results.append(nome)
-			results.append(preco_antigo)
-			results.append(preco)
-			results.append(a_vista)
+            print(results) # Exibe no terminal o array results
 
-			print(results) # Exibe no terminal o array results
+            #avaliar(preco, a_vista)
 
-			ws.append(results) # Escreve na tabela o conteúdo do array results
+            ws.append(results) # Escreve na tabela o conteúdo do array results
 
-			wb.save(planilha) # Salva o arquivo .xlsx
+            wb.save(planilha) # Salva o arquivo .xlsx
 
-		a1.close()
+        a1.close()
 
 
-	except Exception as e2:
-		print(red + "[!] Erro: {}" .format(e2) + restore)
-		pass
+    except Exception as e2:
+        print(red + "[!] Erro: {}" .format(e2) + restore)
+        pass
+
+
+def main():
+
+    global planilha, nome_aba, nome_arquivo, ws, wb
+
+
+    planilha = str(input("Insira o nome da planilha (Não esqueça de colocar a extensão .xlsx)\n"))
+
+    try:
+        wb = load_workbook(planilha) # Carrega um arquivo xlsx existente
+
+    except FileNotFoundError:
+        print(green + "[+] Criando planilha. . ." + restore)
+        cria_planilha.Planilha(planilha).criar()
+        wb = load_workbook(planilha) # Carrega um arquivo xlsx existente
+
+    opcao = str.lower(input("[*] Deseja criar uma nova aba para inserir os dados? [S/N/C]\n"))
+
+    if opcao == 's':
+        nome_aba = str(input("Digite um nome para a nova aba: "))
+        ws = wb.create_sheet(nome_aba)
+
+    elif opcao == 'n':
+        nome_aba = str(input("Digite o nome da aba: "))
+        ws = wb.get_sheet_by_name(nome_aba)
+
+    else:
+        print(yellow + "[!] Escrevendo em uma aba existente. . ." + restore)
+        ws = wb.active
+
+    nome_arquivo = str(input("Nome do arquivo com os links (Não esqueça de colocar a extensão .txt)\n"))
+    print(green + '[+] Iniciando' + restore)
+    time.sleep(2)
 
 
 if __name__ == "__main__":
 
-	opcao = str.lower(input('>> '))
+    opcao = str.lower(input('>> '))
 
-	if opcao == 'help' or opcao == 'h':
-		print(
-	"""
-	O programa abrirá um arquivo de texto com os links referentes ao site e fará a extração dos
-	dados contidos nos links de acordo com os parâmetros de extração (nome do produto, preço).
-	Esses dados serão jogados em uma planilha que o programa irá criar caso a planilha não exista.
-	"""
-	)
+    if opcao == 'help' or opcao == 'h':
+        print(
+    """
+    O programa abrirá um arquivo de texto com os links referentes ao site e fará a extração dos
+    dados contidos nos links de acordo com os parâmetros de extração (nome do produto, preço).
+    Esses dados serão jogados em uma planilha que o programa irá criar caso a planilha não exista.
+    """
+    )
 
-	elif opcao == 's':
-		main()
+    elif opcao == 's':
+        main()
 
-	elif opcao == '0':
-		exit(0)
+    elif opcao == '0':
+        exit(0)
 
-	print(yellow + negrito + '[+] Ctrl + C para parar o programa.' + restore)
-	while True:
-		extrair()
-		time.sleep(600)
+    print(yellow + negrito + '[+] Ctrl + C para parar o programa.' + restore)
+    while True:
+        extrair()
+        time.sleep(600)
