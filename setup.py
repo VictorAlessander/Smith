2dfa9aeac064aae70b0ad9ba2693f36ef3db6696
Victor Alessander <alessander@protonmail.com>
2021-06-20 18:55:51 -0300

    packing smith

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'smith',
+    packages = ['smith'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A webscraper with a sofisticated toolkit to scrap the world',
+    author = 'Victor Alessander',
+    author_email = 'victor.alessander.gr@gmail.com',
+    url = 'https://github.com/VictorAlessander/Smith',
+    download_url = '',
+    keywords = ['crawler', 'webscraping'],
+    classifiers = [],
+)
